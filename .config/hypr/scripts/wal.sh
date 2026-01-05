c3e770457a7331d5d3d5a37932b2bdd666edde52
ナッティ <furytorch.1861@gmail.com>
2026-01-06 06:34:58 +0700

    hyprland: update wallpaper script to work with hyprland v0.53.x

--- .config/hypr/scripts/wal.sh
+++ .config/hypr/scripts/wal.sh
@@ -6,11 +6,13 @@ trap 'rm -f "$tmp_file"' EXIT
 kitty --class 'wallpaper-picker' yazi "$HOME/Pictures/Wallpapers" --chooser-file="$tmp_file"
 
 if [ -s "$tmp_file" ]; then
-    wallpaper_path=$(cat "$tmp_file")
+  wallpaper_path=$(cat "$tmp_file")
 
-    if [ -n "$wallpaper_path" ] && [ -f "$wallpaper_path" ]; then
-        echo -e "preload = ${wallpaper_path}\nwallpaper = , ${wallpaper_path}" > ~/.config/hypr/hyprpaper.conf
-        pkill hyprpaper | true && hyprpaper & disown
-        ln -sf "${wallpaper_path}" ~/.config/current_wallpaper
-    fi
+  if [ -n "$wallpaper_path" ] && [ -f "$wallpaper_path" ]; then
+    echo -e "wallpaper {\nmonitor = \npath = ${wallpaper_path}\nfit_mode = cover\n}\n" >~/.config/hypr/hyprpaper.conf
+    pkill hyprpaper | true && hyprpaper &
+    disown
+    ln -sf "${wallpaper_path}" ~/.config/current_wallpaper
+  fi
 fi
+
