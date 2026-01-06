1a4c9ce26329c7b66b416868ae120075747573ae
ナッティ <furytorch.1861@gmail.com>
2026-01-06 19:24:37 +0700

    hyprland: fix hyprpaper config error

--- .config/hypr/scripts/wal.sh
+++ .config/hypr/scripts/wal.sh
@@ -9,10 +9,9 @@ if [ -s "$tmp_file" ]; then
   wallpaper_path=$(cat "$tmp_file")
 
   if [ -n "$wallpaper_path" ] && [ -f "$wallpaper_path" ]; then
-    echo -e "wallpaper {\nmonitor = \npath = ${wallpaper_path}\nfit_mode = cover\n}\n" >~/.config/hypr/hyprpaper.conf
+    echo -e "splash = false\n\nwallpaper {\n  monitor = \n  path = ${wallpaper_path}\n  fit_mode = cover\n}\n" >~/.config/hypr/hyprpaper.conf
     pkill hyprpaper | true && hyprpaper &
     disown
     ln -sf "${wallpaper_path}" ~/.config/current_wallpaper
   fi
 fi
-
