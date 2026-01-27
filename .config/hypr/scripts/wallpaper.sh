1825daeceee77b72916e761093df0ea3ef472ef4
ナッティ <furytorch.1861@gmail.com>
2026-01-28 06:11:51 +0700

    hypr: add random wallpaper option

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -17,19 +17,27 @@ if [[ $1 == "waybar" ]]; then
   ROFI_CMD+=(-theme-str "${ROFI_WAYBAR_POS}")
 fi
 
-choice=$(find "${WALLPAPER_DIR}" -type f | while read -r file; do
-  filename="${file#"$WALLPAPER_DIR"/}"
+choice=$( {
+  echo -en "Random\0icon\x1fmedia-playlist-shuffle\n"
+  find "${WALLPAPER_DIR}" -type f | while read -r file; do
+    filename="${file#"$WALLPAPER_DIR"/}"
 
-  hash=$(echo -n "$file" | md5sum | cut -d' ' -f1)
-  thumb="${CACHE_DIR}/${hash}.png"
+    hash=$(echo -n "$file" | md5sum | cut -d' ' -f1)
+    thumb="${CACHE_DIR}/${hash}.png"
 
-  if [ ! -f "$thumb" ]; then
-    magick "$file" -thumbnail '500x500>' -strip "$thumb"
-  fi
+    if [ ! -f "$thumb" ]; then
+      magick "$file" -thumbnail '500x500>' -strip "$thumb"
+    fi
 
-  echo -en "${filename}\0icon\x1f${thumb}\n"
-done | "${ROFI_CMD[@]}")
+    echo -en "${filename}\0icon\x1f${thumb}\n"
+  done
+} | "${ROFI_CMD[@]}")
+
+if [[ "$choice" == "Random" ]]; then
+  choice=$(find "${WALLPAPER_DIR}" -type f | shuf -n 1)
+  choice="${choice#"$WALLPAPER_DIR"/}"
+fi
 
 if [[ -n "$choice" ]]; then
-  swww img -t random --transition-duration 2 --transition-step 100 --transition-fps 60 "${WALLPAPER_DIR}"/"${choice}"
+  swww img -t any --transition-duration 2 --transition-step 100 --transition-fps 60 "${WALLPAPER_DIR}"/"${choice}"
 fi
