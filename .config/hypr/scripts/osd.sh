996db0c1667df195f0f56e4ac49444a23a8747e9
ナッティ <furytorch.1861@gmail.com>
2026-02-13 20:07:25 +0700

    hypr: reduce osd timeout

--- .config/hypr/scripts/osd.sh
+++ .config/hypr/scripts/osd.sh
@@ -4,10 +4,10 @@ if [[ "$1" == "--bar" ]]; then
   # Usage: ./osd.sh --bar 50 "Volume"
   value="$2"
   title="$3"
-  notify-send -a "osd" -u low -h "int:value:$value" -h "string:x-canonical-private-synchronous:osd" "$title" ": $value%"
+  notify-send -a "osd" -h "int:value:$value" -h "string:x-canonical-private-synchronous:osd" "$title" ": $value%"
 else
   # Usage: ./osd.sh "Title" "Description"
   title="$1"
   body="$2"
-  notify-send -a "osd" -u low -h "string:x-canonical-private-synchronous:osd" "$title" ": $body"
+  notify-send -a "osd" -h "string:x-canonical-private-synchronous:osd" "$title" ": $body"
 fi
